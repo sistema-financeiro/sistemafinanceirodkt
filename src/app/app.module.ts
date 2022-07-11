d8b0389fafc7e525cdc626dc342be439196dfbfe
Sistema Financeiro - Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-07-11 17:50:43 -0300

    SISTEMAFINANCEIRODKT0211072022130634
    - Feature: Implementar protótipo do projeto (template)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonitoramentoComponent } from './view/monitoramento/monitoramento.component';
+import { HeaderComponent } from './component/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitoramentoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
