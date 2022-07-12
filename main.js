6fd92dc2d25430ab65dc60db96269ab4c06258ce
Sistema Financeiro - Desenvolvimento <repository.desenvolvimento@gmail.com>
2022-07-11 21:08:20 -0300

    SISTEMAFINANCEIRODKT0211072022175332
    - Feature: Implementar protótipo do projeto

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const url = require('url');
 const path = require('path');
 
 function onReady () {     
-	win = new BrowserWindow({width: 1800, height: 1000})
+	win = new BrowserWindow(
+		{
+			width: 1800, 
+			height: 1000,
+			autoHideMenuBar: true,
+		})
 	win.loadURL(url.format({
 		pathname: path.join(
 			__dirname,
